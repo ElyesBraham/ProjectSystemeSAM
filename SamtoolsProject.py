19a7a3abcdbfd6cb20811ac4c0979d730e8c49ff
ElyesBraham <96091237+ElyesBraham@users.noreply.github.com>
2021-12-14 21:13:16 +0100

    Update SamtoolsProject.py

--- SamtoolsProject.py
+++ SamtoolsProject.py
@@ -22,7 +22,7 @@ def analyse() :
 
                 #"http://broadinstitute.github.io/picard/explain-flags.html"
                 
-                #read paired : 0x1
+               
                 #read mapped in proper pair : 0x2
                 #read unmapped : 0x4
                 #mate unmapped : 0x8
@@ -41,19 +41,19 @@ def analyse() :
                 ################################ paires de reads où un seul read de la paire est entièrement mappé et l'autre non mappé
                 
 
-                "samtools view -f 0x1 -f 0x4 -F 0x8 mapping.sorted.bam > read_unmapped_mate_mapped.fasta",
-                "samtools view -f 0x1 -f 0x8 -F 0x4 mapping.sorted.bam > read_mapped__mate_unmapped.fasta",
+                "samtools view  -f 0x4 -F 0x8 mapping.sorted.bam > read_unmapped_mate_mapped.fasta",
+                "samtools view  -f 0x8 -F 0x4 mapping.sorted.bam > read_mapped__mate_unmapped.fasta",
         
-                "samtools view -f 0x1 -f 0x4 -F 0x8 mapping.sorted.bam | echo 'Nombre de paires de read avec le read unmapped et le mate mapped:' $(wc -l) >> summary_file.txt",
-                "samtools view -f 0x1 -f 0x8 -F 0x4 mapping.sorted.bam | echo 'Nombre de paires de read avec le read mapped et le mate unmapped:' $(wc -l) >> summary_file.txt",
+                "samtools view  -f 0x4 -F 0x8 mapping.sorted.bam | echo 'Nombre de paires de read avec le read unmapped et le mate mapped:' $(wc -l) >> summary_file.txt",
+                "samtools view  -f 0x8 -F 0x4 mapping.sorted.bam | echo 'Nombre de paires de read avec le read mapped et le mate unmapped:' $(wc -l) >> summary_file.txt",
                 
                 ################################ paires de reads où un seul read de la paire est entièrement mappé et l'autre partiellement mappé
                 
-                "samtools view -f 0x1 -f 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' > read_mapped__mate_partially_mapped.fasta",
-                "samtools view -f 0x1 -F 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' > read_partially_mapped__mate_mapped.fasta",
+                "samtools view -f 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' > read_mapped__mate_partially_mapped.fasta",
+                "samtools view -F 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' > read_partially_mapped__mate_mapped.fasta",
 
-                "samtools view -f 0x1 -f 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' | echo 'Nombre de paires de read avec le read mapped et le mate partially mapped:' $(wc -l) >> summary_file.txt",
-                "samtools view -f 0x1 -F 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' | echo 'Nombre de paires de read avec le read partially mapped et le mate mapped:' $(wc -l) >> summary_file.txt",
+                "samtools view -f 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' | echo 'Nombre de paires de read avec le read mapped et le mate partially mapped:' $(wc -l) >> summary_file.txt",
+                "samtools view -F 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' | echo 'Nombre de paires de read avec le read partially mapped et le mate mapped:' $(wc -l) >> summary_file.txt",
                 "samtools idxstats mapping.sorted.bam >> summary_file.txt", #résumé statistique des alignements (mappés et non mappés)
                 "rm mapping.sorted.bam mapping.sorted.bam.bai mapping.bam"): #enlever les fichiers inutiles
                 call(cmd, shell=True) 
