392c47314780e1a4f6ac085f372e7987d7b316e3
ElyesBraham <96091237+ElyesBraham@users.noreply.github.com>
2021-12-14 21:52:34 +0100

    Update SamtoolsProject.py

--- SamtoolsProject.py
+++ SamtoolsProject.py
@@ -19,10 +19,7 @@ def analyse() :
                 "samtools index mapping.sorted.bam", # indexage du fichier BAM "sorted"
 
                 ################################ Total reads in SAM file 
-
-                #"http://broadinstitute.github.io/picard/explain-flags.html"
-                
-               
+                            
                 #read mapped in proper pair : 0x2
                 #read unmapped : 0x4
                 #mate unmapped : 0x8
@@ -49,11 +46,11 @@ def analyse() :
                 
                 ################################ paires de reads où un seul read de la paire est entièrement mappé et l'autre partiellement mappé
                 
-                "samtools view -f 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' > read_mapped__mate_partially_mapped.fasta",
-                "samtools view -F 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' > read_partially_mapped__mate_mapped.fasta",
+                "samtools view -f 0x4 -F 0x8 mapping.sorted.bam | grep -v '100M' > read_mapped__mate_partially_mapped.fasta",
+                "samtools view -F 0x4 -F 0x8 mapping.sorted.bam | grep -v '100M' > read_partially_mapped__mate_mapped.fasta",
 
-                "samtools view -f 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' | echo 'Nombre de paires de read avec le read mapped et le mate partially mapped:' $(wc -l) >> summary_file.txt",
-                "samtools view -F 0x2 -F 0x8 mapping.sorted.bam | grep -v '100M' | echo 'Nombre de paires de read avec le read partially mapped et le mate mapped:' $(wc -l) >> summary_file.txt",
+                "samtools view -f 0x4 -F 0x8 mapping.sorted.bam | grep -v '100M' | echo 'Nombre de paires de read avec le read mapped et le mate partially mapped:' $(wc -l) >> summary_file.txt",
+                "samtools view -F 0x4 -F 0x8 mapping.sorted.bam | grep -v '100M' | echo 'Nombre de paires de read avec le read partially mapped et le mate mapped:' $(wc -l) >> summary_file.txt",
                 "samtools idxstats mapping.sorted.bam >> summary_file.txt", #résumé statistique des alignements (mappés et non mappés)
                 "rm mapping.sorted.bam mapping.sorted.bam.bai mapping.bam"): #enlever les fichiers inutiles
                 call(cmd, shell=True) 
